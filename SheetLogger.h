8c4addf476717db6f4781447659157a6f83d6c20
Luke Rymarz <lukerymarz@gmail.com>
2021-04-05 22:22:04 -0700

    include protection

--- SheetLogger.h
+++ SheetLogger.h
@@ -1,3 +1,6 @@
+#ifndef SHEETLOGGER_INCLUDE
+#define SHEETLOGGER_INCLUDE
+
 // Sheet lives here: https://docs.google.com/spreadsheets/d/1s3l9Ynd2_5dL4CWa_6QCBA5sY6KbA35eckuIAtvpB7k/edit#gid=0
 // Script lives here: https://script.google.com/u/0/home/projects/1tvJFLoZ03pvSuns1yMmNIlcFfA2VsJvgA9v-KSM4pQuZkNXn6tQ3L-lh/edit
 #ifndef GSCRIPTID
@@ -6,3 +9,5 @@
 
 bool setupSheetLogger();
 void logTempHumidityToSheet(float temp, float humidity);
+
+#endif
--- Wifi.h
+++ Wifi.h
@@ -1,6 +1,11 @@
+#ifndef WIFI_INCLUDE
+#define WIFI_INCLUDE
+
 #include <SPI.h>
 #include <WiFiNINA.h>
 #include "Secrets.h"
 
 bool connectToWifi();
 long getSignalStrength();
+
+#endif
